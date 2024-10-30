a4adf80f3edf7287a95697a0648047d4e218efb8
Piga962 <cesarjrmecinas@gmail.com>
2024-10-29 19:40:05 -0600

    Changed the register visit function

--- controllers/visitController.js
+++ controllers/visitController.js
@@ -3,8 +3,16 @@ const expositionModel = require('../models/expositionModel');
 const progressZoneModel = require('../models/progressZoneModel');
 
 async function registerVisit(req, res){
-    const {id_usuario, id_exposicion} = req.body;
+    const id_usuario = req.user.id_usuario;
+    const {contenido_qr} = req.body;
     try{
+        const exposition = await visitModel.findExposicionByQr(contenido_qr);
+        if(!exposition){
+            return res.status(404).send('Exposición no encontrada con el código QR proporcionado');
+        }
+
+        const id_exposicion = exposition.id_exposicion;
+
         const newVisit = await visitModel.registerVisit({id_usuario, id_exposicion});
         res.status(201).send(newVisit);
     } catch(error){
--- models/visitModel.js
+++ models/visitModel.js
@@ -2,7 +2,7 @@ const {db} = require('../database/db');
 
 async function registerVisit({id_usuario, id_exposicion}){
     try{
-        const query = 'INSERT INTO visita (id_usuario, id_exposicion) VALUES ($1, $2) RETURNING *;';
+        const query = 'INSERT INTO visita (id_usuario, id_exposicion,fecha_hora_visita) VALUES ($1, $2, NOW()) RETURNING *;';
         const {rows} = await db.query(query, [id_usuario, id_exposicion]);
         return rows[0];
     } catch(error){
@@ -11,4 +11,15 @@ async function registerVisit({id_usuario, id_exposicion}){
     }
 }
 
-module.exports = {registerVisit}
+async function findExposicionByQr(codigo_qr){
+    try{
+        const query = 'SELECT * FROM exposicion WHERE codigo_qr = $1;';
+        const {rows} = await db.query(query, [codigo_qr]);
+        return rows[0];
+    } catch(error){
+        console.error('Error al buscar exposición por QR:', error);
+        throw error;
+    }
+}
+
+module.exports = {registerVisit,findExposicionByQr}
