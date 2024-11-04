30d44465d83c17d03afcb89ae28755993ec918c0
Piga962 <cesarjrmecinas@gmail.com>
2024-11-04 01:29:14 -0600

    Updated database connection

--- database/db.js
+++ database/db.js
@@ -5,8 +5,8 @@ const db = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: 5432,
-    database: 'MuseoPapalote',
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
 })
 
 module.exports = {db};
