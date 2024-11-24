6fbb3cada7b0b35be5f44b409a6354de327e3aa0
Piga962 <cesarjrmecinas@gmail.com>
2024-11-24 17:29:04 -0600

    Corrected more documentation

--- controllers/respuestaTriviaController.js
+++ controllers/respuestaTriviaController.js
@@ -5,7 +5,7 @@ async function createRespuestaTrivia(req, res){
     const {id_pregunta, opcion_seleccionada} = req.body;
     try {
         const nuevaRespuesta = await respuestaTriviaModel.createRespuestaTrivia(id_pregunta, id_usuario, opcion_seleccionada);
-        res.status(201).json({message: 'Respuesta creada exitosamente', respuesta: nuevaRespuesta});
+        res.status(201).json(nuevaRespuesta);
     } catch (error) {
         console.error('Error al crear respuesta:', error);
         res.status(500).json({message: 'Error al crear respuesta'});
